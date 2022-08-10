7cf02055df91e2aecf75376b71f404faec41b827
Vinícius <viniciusmvn@pm.me>
2022-08-09 22:12:22 -0300

    feat: add dotenv

--- cmd/main.go
+++ cmd/main.go
@@ -7,36 +7,29 @@ import (
 	"os"
 
 	"github.com/BertBR/RetroGamesBot-Go/cmd/bot"
-	"github.com/BertBR/RetroGamesBot-Go/cmd/service"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := run()
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		return
+	}
+	pool := run()
 	if err != nil {
 		log.Fatal(err)
 	}
-	bot.New()
+	bot.New(pool)
 }
 
-func run() error {
+func run() *pgxpool.Pool {
 	ctx := context.Background()
 	pool, err := pgxpool.Connect(ctx, os.Getenv("DATABASE_URL"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
-	defer pool.Close()
-	svc := service.New(pool)
-	top10Console, err := svc.GetTop10Console(ctx)
-	if err != nil {
-		return err
-	}
-	top10Genre, err := svc.GetTop10Genre(ctx)
-	if err != nil {
-		return err
-	}
-	fmt.Println(top10Genre)
-	fmt.Println(top10Console)
-	return nil
+	return pool
 }

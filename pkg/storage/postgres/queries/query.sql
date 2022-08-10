8a39d80efa7deb17f971e6f709f4fa98bbfd8a20
Vinícius <viniciusmvn@pm.me>
2022-08-09 22:11:20 -0300

    feat(database): add sqlc config with schema and queries

--- pkg/storage/postgres/queries/query.sql
+++ pkg/storage/postgres/queries/query.sql
@@ -1,13 +1,41 @@
 -- name: GetTotalSortedByConsole :many
-SELECT console, sum("public"."games"."sorted") AS "sum"
-    FROM "public"."games"
-    GROUP BY console
-    ORDER BY console DESC
-    LIMIT 10;
-    
+SELECT
+    "console",
+    sum("public"."games"."sorted") AS "sum"
+FROM
+    "public"."games"
+GROUP BY
+    "console"
+ORDER BY
+    "sum" DESC,
+    "console" ASC
+LIMIT 10;
+
 -- name: GetTotalSortedByGenre :many
-SELECT genre, sum("public"."games"."sorted") AS "sum"
-    FROM "public"."games"
-    GROUP BY genre
-    ORDER BY genre DESC
-    LIMIT 10;
+SELECT
+    "genre",
+    sum("public"."games"."sorted") AS "sum"
+FROM
+    "public"."games"
+GROUP BY
+    "genre"
+ORDER BY
+    "sum" DESC,
+    "genre" ASC
+LIMIT 10;
+
+-- name: GetTop10Games :many
+SELECT
+    "title",
+    "image_url",
+    sum("public"."games"."sorted") AS "sum"
+FROM
+    "public"."games"
+GROUP BY
+    "title",
+    "image_url"
+ORDER BY
+    "sum" DESC,
+    "title" ASC
+LIMIT 10;
+

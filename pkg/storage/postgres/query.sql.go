1997d518c913c6199ae9c172e37b3147e6a86d94
Vinícius <viniciusmvn@pm.me>
2022-08-09 22:12:10 -0300

    feat: improve sqlc queries

--- pkg/storage/postgres/query.sql.go
+++ pkg/storage/postgres/query.sql.go
@@ -9,12 +9,60 @@ import (
 	"context"
 )
 
+const getTop10Games = `-- name: GetTop10Games :many
+SELECT
+    "title",
+    "image_url",
+    sum("public"."games"."sorted") AS "sum"
+FROM
+    "public"."games"
+GROUP BY
+    "title",
+    "image_url"
+ORDER BY
+    "sum" DESC,
+    "title" ASC
+LIMIT 10
+`
+
+type GetTop10GamesRow struct {
+	Title    string
+	ImageUrl string
+	Sum      int64
+}
+
+func (q *Queries) GetTop10Games(ctx context.Context) ([]GetTop10GamesRow, error) {
+	rows, err := q.db.Query(ctx, getTop10Games)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []GetTop10GamesRow
+	for rows.Next() {
+		var i GetTop10GamesRow
+		if err := rows.Scan(&i.Title, &i.ImageUrl, &i.Sum); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
 const getTotalSortedByConsole = `-- name: GetTotalSortedByConsole :many
-SELECT console, sum("public"."games"."sorted") AS "sum"
-    FROM "public"."games"
-    GROUP BY console
-    ORDER BY console DESC
-    LIMIT 10
+SELECT
+    "console",
+    sum("public"."games"."sorted") AS "sum"
+FROM
+    "public"."games"
+GROUP BY
+    "console"
+ORDER BY
+    "sum" DESC,
+    "console" ASC
+LIMIT 10
 `
 
 type GetTotalSortedByConsoleRow struct {
@@ -43,11 +91,17 @@ func (q *Queries) GetTotalSortedByConsole(ctx context.Context) ([]GetTotalSorted
 }
 
 const getTotalSortedByGenre = `-- name: GetTotalSortedByGenre :many
-SELECT genre, sum("public"."games"."sorted") AS "sum"
-    FROM "public"."games"
-    GROUP BY genre
-    ORDER BY genre DESC
-    LIMIT 10
+SELECT
+    "genre",
+    sum("public"."games"."sorted") AS "sum"
+FROM
+    "public"."games"
+GROUP BY
+    "genre"
+ORDER BY
+    "sum" DESC,
+    "genre" ASC
+LIMIT 10
 `
 
 type GetTotalSortedByGenreRow struct {
